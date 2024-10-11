47ca2c69ed20b9e458b79aea29280a5de7de5117
Lukas Smyczek <slukasp@outlook.de>
2024-10-11 10:45:27 +0200

    OperationVisitor zum ast hinzugefügt

---
+++ src/main/java/de/hhu/stups/codegenerator/ast/adapter/OperationVisitor.java
@@ -0,0 +1,22 @@
+package de.hhu.stups.codegenerator.ast.adapter;
+
+import de.be4.classicalb.core.parser.node.Node;
+import de.prob.parser.ast.SourceCodePosition;
+import de.prob.parser.ast.nodes.OperationNode;
+
+public class OperationVisitor extends AbstractVisitor{
+
+    private OperationNode resultOperationNode;
+    
+    public OperationNode getResult(){
+        return resultOperationNode;
+    }
+
+    private SourceCodePosition getSourceCodePosition(Node node) {
+        SourceCodePosition sourceCodePosition = new SourceCodePosition();
+        sourceCodePosition.setStartColumn(node.getStartPos().getPos());
+        sourceCodePosition.setStartLine(node.getStartPos().getLine());
+        sourceCodePosition.setText(node.toString().replace(" ", ""));
+        return sourceCodePosition;
+    }
+}
