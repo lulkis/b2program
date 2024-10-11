b60f6400d845e92f35c8fefb497df4c325dd753c
Lukas Smyczek <slukasp@outlook.de>
2024-10-11 10:38:41 +0200

    Hinzufügen einer getSourceCodePosition im ExpressionVisitor

--- src/main/java/de/hhu/stups/codegenerator/ast/adapter/ExpressionVisitor.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/adapter/ExpressionVisitor.java
@@ -1,5 +1,7 @@
 package de.hhu.stups.codegenerator.ast.adapter;
 
+import de.be4.classicalb.core.parser.node.Node;
+import de.prob.parser.ast.SourceCodePosition;
 import de.prob.parser.ast.nodes.expression.ExprNode;
 
 public class ExpressionVisitor extends AbstractVisitor{
@@ -8,5 +10,13 @@ public class ExpressionVisitor extends AbstractVisitor{
 
     public ExprNode getResult() {
         return resultExpressionNode;
+    }
+
+    private SourceCodePosition getSourceCodePosition(Node node) {
+        SourceCodePosition sourceCodePosition = new SourceCodePosition();
+        sourceCodePosition.setStartColumn(node.getStartPos().getPos());
+        sourceCodePosition.setStartLine(node.getStartPos().getLine());
+        sourceCodePosition.setText(node.toString().replace(" ", ""));
+        return sourceCodePosition;
     }
 }
