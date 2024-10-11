9bb895f4a61f52c55296d3769834d720b6359a9f
Lukas Smyczek <slukasp@outlook.de>
2024-10-11 15:19:35 +0200

    Adding conversion for MachineNode in VisitorCoordinator

--- src/main/java/de/hhu/stups/codegenerator/ast/VisitorCoordinator.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/VisitorCoordinator.java
@@ -1,4 +1,14 @@
 package de.hhu.stups.codegenerator.ast;
 
+import de.be4.classicalb.core.parser.node.Start;
+import de.hhu.stups.codegenerator.ast.adapter.MachineVisitor;
+import de.prob.parser.ast.nodes.MachineNode;
+
 public class VisitorCoordinator {
+
+    public MachineNode convertMachineNode(Start start){
+        MachineVisitor visitor = new MachineVisitor();
+        start.apply(visitor);
+        return visitor.getResult();
+    }
 }
