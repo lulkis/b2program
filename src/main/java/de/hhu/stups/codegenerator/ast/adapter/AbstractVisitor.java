60584909edcd87d2a900fa91c18bd0d012c7de40
Lukas Smyczek <slukasp@outlook.de>
2024-10-11 14:51:31 +0200

    Extending AbstractVisitor with DepthFirstAdapter

--- src/main/java/de/hhu/stups/codegenerator/ast/adapter/AbstractVisitor.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/adapter/AbstractVisitor.java
@@ -1,8 +1,9 @@
 package de.hhu.stups.codegenerator.ast.adapter;
 
+import de.be4.classicalb.core.parser.analysis.DepthFirstAdapter;
 import de.prob.parser.ast.nodes.Node;
 
-public abstract class AbstractVisitor {
+public abstract class AbstractVisitor extends DepthFirstAdapter {
     private Node result;
 
     public abstract Node getResult();
