4bbca895f62d0d56e8de38d37a6e3dbeb660e294
Lukas Smyczek <slukasp@outlook.de>
2024-10-11 10:29:55 +0200

    Den AbstractVisitor zu einer abstrakten Klasse gemacht und Methode hinzugefügt zur Rückgabe einer Node

--- src/main/java/de/hhu/stups/codegenerator/ast/adapter/AbstractVisitor.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/adapter/AbstractVisitor.java
@@ -1,2 +1,9 @@
-package de.hhu.stups.codegenerator.ast.adapter;public class AbstractVisitor {
+package de.hhu.stups.codegenerator.ast.adapter;
+
+import de.prob.parser.ast.nodes.Node;
+
+public abstract class AbstractVisitor {
+    private Node result;
+
+    public abstract Node getResult();
 }
