833ece176d026808d448646ce484b0f5f788ced1
Lukas Smyczek <slukasp@outlook.de>
2024-10-11 10:41:02 +0200

    PredicateVisitor zum ast hinzugefügt

---
+++ src/main/java/de/hhu/stups/codegenerator/ast/adapter/PredicateVisitor.java
@@ -0,0 +1,22 @@
+package de.hhu.stups.codegenerator.ast.adapter;
+
+import de.be4.classicalb.core.parser.node.Node;
+import de.prob.parser.ast.SourceCodePosition;
+import de.prob.parser.ast.nodes.predicate.PredicateNode;
+
+public class PredicateVisitor  extends AbstractVisitor{
+
+    private PredicateNode resultPredicateNode;
+
+    public PredicateNode getResult(){
+        return resultPredicateNode;
+    }
+
+    private SourceCodePosition getSourceCodePosition(Node node) {
+        SourceCodePosition sourceCodePosition = new SourceCodePosition();
+        sourceCodePosition.setStartColumn(node.getStartPos().getPos());
+        sourceCodePosition.setStartLine(node.getStartPos().getLine());
+        sourceCodePosition.setText(node.toString().replace(" ", ""));
+        return sourceCodePosition;
+    }
+}
