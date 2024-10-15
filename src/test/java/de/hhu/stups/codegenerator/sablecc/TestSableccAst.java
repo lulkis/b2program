e75d16f1d3abfa9f0f923692d0a1821e76a4dd1d
Lukas Smyczek <slukasp@outlook.de>
2024-10-15 14:12:46 +0200

    Adding test for Nota Machine Name

--- src/test/java/de/hhu/stups/codegenerator/sablecc/TestSableccAst.java
+++ src/test/java/de/hhu/stups/codegenerator/sablecc/TestSableccAst.java
@@ -94,4 +94,20 @@ public class TestSableccAst {
         assertEquals(antlrMachineNode.getMachineNode("Lift").getOperations().get(0).toString(),
                 machineNode.getOperations().get(0).toString());
     }
+
+    @Test
+    public void testCorrectMachineNameNota() throws Exception{
+        BParser parser = new BParser();
+        Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
+                .getResource("de/hhu/stups/codegenerator/sablecc/nota.mch").toURI());
+        Start start = parser.parseFile(mchPath.toFile());
+        VisitorCoordinator coordinator = new VisitorCoordinator();
+        MachineNode machineNode;
+
+        //BProject antlrMachineNode  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
+        machineNode = coordinator.convertMachineNode(start);
+
+        assertEquals("nota",
+                machineNode.getName());
+    }
 }
