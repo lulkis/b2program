79ca21b2a4c3ed61e1168eb8390b2e63fd852dd8
Lukas Smyczek <slukasp@outlook.de>
2024-10-15 11:31:28 +0200

    Adding support for Operation Node in OperationVisitor

--- src/main/java/de/hhu/stups/codegenerator/ast/adapter/OperationVisitor.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/adapter/OperationVisitor.java
@@ -1,15 +1,45 @@
 package de.hhu.stups.codegenerator.ast.adapter;
 
+import de.be4.classicalb.core.parser.node.AOperation;
 import de.be4.classicalb.core.parser.node.Node;
+import de.be4.classicalb.core.parser.node.PExpression;
+import de.hhu.stups.codegenerator.ast.VisitorCoordinator;
 import de.prob.parser.ast.SourceCodePosition;
+import de.prob.parser.ast.nodes.DeclarationNode;
+import de.prob.parser.ast.nodes.MachineNode;
 import de.prob.parser.ast.nodes.OperationNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OperationVisitor extends AbstractVisitor{
 
     private OperationNode resultOperationNode;
+    private VisitorCoordinator coordinator = new VisitorCoordinator();
+    private MachineNode machineNode;
+
+    public OperationVisitor(MachineNode machineNode) {
+        this.machineNode = machineNode;
+    }
     
     public OperationNode getResult(){
         return resultOperationNode;
+    }
+
+    @Override
+    public void caseAOperation(AOperation node){
+        List<DeclarationNode> paramNodes = new ArrayList<>();
+        for(PExpression expression : node.getParameters()){
+            paramNodes.add(new DeclarationNode(getSourceCodePosition(expression),
+                    expression.toString().replace(" ", ""),
+                    DeclarationNode.Kind.OP_INPUT_PARAMETER,
+                    machineNode));
+        }
+        resultOperationNode = new OperationNode(getSourceCodePosition(node),
+                node.getOpName().get(0).getText(),
+                new ArrayList<>(),
+                coordinator.convertSubstitutionNode(node.getOperationBody()),
+                paramNodes);
     }
 
     private SourceCodePosition getSourceCodePosition(Node node) {
