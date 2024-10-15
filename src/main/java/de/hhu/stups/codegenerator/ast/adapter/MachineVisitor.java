31abdf6f0c91f910f38e3c50dc17a12055c3b75d
Lukas Smyczek <slukasp@outlook.de>
2024-10-15 09:28:39 +0200

    Adding Variables needed for the MachineNode to the MachineVisitor

--- src/main/java/de/hhu/stups/codegenerator/ast/adapter/MachineVisitor.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/adapter/MachineVisitor.java
@@ -2,11 +2,27 @@ package de.hhu.stups.codegenerator.ast.adapter;
 
 import de.be4.classicalb.core.parser.node.Node;
 import de.prob.parser.ast.SourceCodePosition;
+import de.prob.parser.ast.nodes.DeclarationNode;
+import de.prob.parser.ast.nodes.EnumeratedSetDeclarationNode;
 import de.prob.parser.ast.nodes.MachineNode;
+import de.prob.parser.ast.nodes.OperationNode;
+import de.prob.parser.ast.nodes.predicate.PredicateNode;
+import de.prob.parser.ast.nodes.substitution.SubstitutionNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MachineVisitor extends AbstractVisitor{
 
     private MachineNode resultMachineNode;
+
+    private String name;
+    private List<DeclarationNode> variables = new ArrayList<>();
+    private PredicateNode invariant;
+    private SubstitutionNode initialisation;
+    private List<OperationNode> operations;
+    private List<EnumeratedSetDeclarationNode> setEnumerations = new ArrayList<>();
+
 
     public MachineNode getResult(){
         return resultMachineNode;
