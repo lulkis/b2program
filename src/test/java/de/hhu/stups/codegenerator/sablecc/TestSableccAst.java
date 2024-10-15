565c4680f3ed52018457567289fed5bb44a7cd94
Lukas Smyczek <slukasp@outlook.de>
2024-10-15 10:25:29 +0200

    Adding Variables Test for the SableCC AST

--- src/test/java/de/hhu/stups/codegenerator/sablecc/TestSableccAst.java
+++ src/test/java/de/hhu/stups/codegenerator/sablecc/TestSableccAst.java
@@ -1,2 +1,29 @@
-package de.hhu.stups.codegenerator.sablecc;public class TestSableccAst {
+package de.hhu.stups.codegenerator.sablecc;
+
+import de.be4.classicalb.core.parser.BParser;
+import de.be4.classicalb.core.parser.node.Start;
+import de.hhu.stups.codegenerator.CodeGenerator;
+import de.hhu.stups.codegenerator.ast.VisitorCoordinator;
+import de.prob.parser.ast.nodes.MachineNode;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TestSableccAst {
+
+    @Test
+    public void testVariables() throws Exception {
+        BParser parser = new BParser();
+        Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
+                .getResource("de/hhu/stups/codegenerator/sablecc/Lift.mch").toURI());
+        Start start = parser.parseFile(mchPath.toFile());
+        VisitorCoordinator coordinator = new VisitorCoordinator();
+        MachineNode machineNode;
+
+        machineNode = coordinator.convertMachineNode(start);
+
+        assertEquals(1, machineNode.getVariables().size());
+    }
 }
