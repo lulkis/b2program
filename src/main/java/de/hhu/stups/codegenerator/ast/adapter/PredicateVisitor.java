86668184cc7be4f86a49f4288b04ace48b075b16
Lukas Smyczek <slukasp@outlook.de>
2024-10-15 11:01:53 +0200

    Adding support for Member Node

--- src/main/java/de/hhu/stups/codegenerator/ast/adapter/PredicateVisitor.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/adapter/PredicateVisitor.java
@@ -1,15 +1,34 @@
 package de.hhu.stups.codegenerator.ast.adapter;
 
+import de.be4.classicalb.core.parser.node.AMemberPredicate;
 import de.be4.classicalb.core.parser.node.Node;
+import de.hhu.stups.codegenerator.ast.VisitorCoordinator;
 import de.prob.parser.ast.SourceCodePosition;
+import de.prob.parser.ast.nodes.expression.ExprNode;
 import de.prob.parser.ast.nodes.predicate.PredicateNode;
+import de.prob.parser.ast.nodes.predicate.PredicateOperatorWithExprArgsNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PredicateVisitor  extends AbstractVisitor{
 
     private PredicateNode resultPredicateNode;
+    private VisitorCoordinator coordinator = new VisitorCoordinator();
 
     public PredicateNode getResult(){
         return resultPredicateNode;
+    }
+
+    @Override
+    public void caseAMemberPredicate(AMemberPredicate node) {
+        List<ExprNode> list = new ArrayList<>();
+        list.add(coordinator.convertExpressionNode(node.getLeft()));
+        list.add(coordinator.convertExpressionNode(node.getRight()));
+        resultPredicateNode = new PredicateOperatorWithExprArgsNode(
+                getSourceCodePosition(node),
+                PredicateOperatorWithExprArgsNode.PredOperatorExprArgs.ELEMENT_OF,
+                list);
     }
 
     private SourceCodePosition getSourceCodePosition(Node node) {
