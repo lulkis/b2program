90982d96fba69c2445e256cb21634085f1d4212b
Lukas Smyczek <slukasp@outlook.de>
2024-10-15 09:22:38 +0200

    Adding ConvertPredicateNode Method to the VisitorCoordinator

--- src/main/java/de/hhu/stups/codegenerator/ast/VisitorCoordinator.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/VisitorCoordinator.java
@@ -1,10 +1,13 @@
 package de.hhu.stups.codegenerator.ast;
 
+import de.be4.classicalb.core.parser.node.PPredicate;
 import de.be4.classicalb.core.parser.node.PSubstitution;
 import de.be4.classicalb.core.parser.node.Start;
 import de.hhu.stups.codegenerator.ast.adapter.MachineVisitor;
+import de.hhu.stups.codegenerator.ast.adapter.PredicateVisitor;
 import de.hhu.stups.codegenerator.ast.adapter.SubstitutionVisitor;
 import de.prob.parser.ast.nodes.MachineNode;
+import de.prob.parser.ast.nodes.predicate.PredicateNode;
 import de.prob.parser.ast.nodes.substitution.SubstitutionNode;
 
 public class VisitorCoordinator {
@@ -17,6 +20,12 @@ public class VisitorCoordinator {
 
     public SubstitutionNode convertSubstitutionNode(PSubstitution node){
         SubstitutionVisitor visitor = new SubstitutionVisitor();
+        node.apply(visitor);
+        return visitor.getResult();
+    }
+
+    public PredicateNode convertPredicateNode(PPredicate node){
+        PredicateVisitor visitor = new PredicateVisitor();
         node.apply(visitor);
         return visitor.getResult();
     }
