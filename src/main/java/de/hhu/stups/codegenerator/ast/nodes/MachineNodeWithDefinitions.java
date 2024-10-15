0263a8a976fbeef250a99bdc7c24453385eb9950
Lukas Smyczek <slukasp@outlook.de>
2024-10-15 17:41:55 +0200

    Adding a new MachineNode that contains all Definitions

--- src/main/java/de/hhu/stups/codegenerator/ast/nodes/MachineNodeWithDefinitions.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/nodes/MachineNodeWithDefinitions.java
@@ -1,2 +1,22 @@
-package de.hhu.stups.codegenerator.ast.nodes;public class MachineNodeWithDefinitions {
+package de.hhu.stups.codegenerator.ast.nodes;
+
+import de.be4.classicalb.core.parser.IDefinitions;
+import de.prob.parser.ast.SourceCodePosition;
+import de.prob.parser.ast.nodes.MachineNode;
+
+public class MachineNodeWithDefinitions extends MachineNode {
+
+    private IDefinitions iDefinitions;
+
+    public MachineNodeWithDefinitions(SourceCodePosition sourceCodePosition) {
+        super(sourceCodePosition);
+    }
+
+    public void setIDefinitions(IDefinitions iDefinitions){
+        this.iDefinitions = iDefinitions;
+    }
+
+    public IDefinitions getIDefinition(){
+        return iDefinitions;
+    }
 }
