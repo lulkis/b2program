b4728badc28e5e5899fbea8524faa4a1a0b920a8
Lukas Smyczek <slukasp@outlook.de>
2024-10-15 09:55:58 +0200

    Adding Support for Variables, Constans and MachineHeader to the MachineVisitor

--- src/main/java/de/hhu/stups/codegenerator/ast/adapter/MachineVisitor.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/adapter/MachineVisitor.java
@@ -1,6 +1,6 @@
 package de.hhu.stups.codegenerator.ast.adapter;
 
-import de.be4.classicalb.core.parser.node.Node;
+import de.be4.classicalb.core.parser.node.*;
 import de.prob.parser.ast.SourceCodePosition;
 import de.prob.parser.ast.nodes.DeclarationNode;
 import de.prob.parser.ast.nodes.EnumeratedSetDeclarationNode;
@@ -23,10 +23,41 @@ public class MachineVisitor extends AbstractVisitor{
     private List<OperationNode> operations;
     private List<EnumeratedSetDeclarationNode> setEnumerations = new ArrayList<>();
 
-
     public MachineNode getResult(){
         return resultMachineNode;
     }
+
+    @Override
+    public void caseAMachineHeader(AMachineHeader node){
+        this.resultMachineNode = new MachineNode(getSourceCodePosition(node));
+        name = node.getName().get(0).toString().replace(" ", "");
+    }
+
+    @Override
+    public void caseAConstantsMachineClause(AConstantsMachineClause node){
+        List<DeclarationNode> constantList = new ArrayList<>();
+        for(PExpression expression : node.getIdentifiers()){
+            constantList.add(new DeclarationNode(getSourceCodePosition(node),
+                    expression.toString().replace(" ", ""),
+                    DeclarationNode.Kind.CONSTANT,
+                    resultMachineNode));
+        }
+        resultMachineNode.addConstants(constantList);
+    }
+
+    @Override
+    public void caseAVariablesMachineClause(AVariablesMachineClause node){
+        List<DeclarationNode> constantList = new ArrayList<>();
+        for(PExpression expression : node.getIdentifiers()){
+            constantList.add(new DeclarationNode(getSourceCodePosition(node),
+                    expression.toString().replace(" ", ""),
+                    DeclarationNode.Kind.VARIABLE,
+                    resultMachineNode));
+        }
+        resultMachineNode.addConstants(constantList);
+    }
+
+
 
     private SourceCodePosition getSourceCodePosition(Node node) {
         SourceCodePosition sourceCodePosition = new SourceCodePosition();
