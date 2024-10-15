8a0b77aca92707c1c1ee4389166f7720d9f21e83
Lukas Smyczek <slukasp@outlook.de>
2024-10-15 10:44:26 +0200

    Adding support for SableCC Nodes (Identifier, Integer, Assign)

--- src/main/java/de/hhu/stups/codegenerator/ast/adapter/ExpressionVisitor.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/adapter/ExpressionVisitor.java
@@ -1,8 +1,14 @@
 package de.hhu.stups.codegenerator.ast.adapter;
 
 import de.be4.classicalb.core.parser.node.Node;
+import de.be4.classicalb.core.parser.node.TIdentifierLiteral;
+import de.be4.classicalb.core.parser.node.TIntegerLiteral;
 import de.prob.parser.ast.SourceCodePosition;
 import de.prob.parser.ast.nodes.expression.ExprNode;
+import de.prob.parser.ast.nodes.expression.IdentifierExprNode;
+import de.prob.parser.ast.nodes.expression.NumberNode;
+
+import java.math.BigInteger;
 
 public class ExpressionVisitor extends AbstractVisitor{
 
@@ -10,6 +16,16 @@ public class ExpressionVisitor extends AbstractVisitor{
 
     public ExprNode getResult() {
         return resultExpressionNode;
+    }
+
+    @Override
+    public void caseTIdentifierLiteral(TIdentifierLiteral node){
+        resultExpressionNode = new IdentifierExprNode(getSourceCodePosition(node), node.getText(), false);
+    }
+
+    @Override
+    public void caseTIntegerLiteral(TIntegerLiteral node){
+        resultExpressionNode = new NumberNode(getSourceCodePosition(node), new BigInteger(node.getText()));
     }
 
     private SourceCodePosition getSourceCodePosition(Node node) {
--- src/main/java/de/hhu/stups/codegenerator/ast/adapter/SubstitutionVisitor.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/adapter/SubstitutionVisitor.java
@@ -1,15 +1,29 @@
 package de.hhu.stups.codegenerator.ast.adapter;
 
+import de.be4.classicalb.core.parser.node.AAssignSubstitution;
 import de.be4.classicalb.core.parser.node.Node;
+import de.hhu.stups.codegenerator.ast.VisitorCoordinator;
 import de.prob.parser.ast.SourceCodePosition;
+import de.prob.parser.ast.nodes.expression.ExprNode;
+import de.prob.parser.ast.nodes.substitution.AssignSubstitutionNode;
 import de.prob.parser.ast.nodes.substitution.SubstitutionNode;
+
+import java.util.List;
 
 public class SubstitutionVisitor extends AbstractVisitor{
 
     private SubstitutionNode resultSubstitutionNode;
+    private VisitorCoordinator coordinator = new VisitorCoordinator();
 
     public SubstitutionNode getResult(){
         return resultSubstitutionNode;
+    }
+
+    @Override
+    public void caseAAssignSubstitution(AAssignSubstitution node){
+        List<ExprNode> left = coordinator.convertExpressionNode(node.getLhsExpression());
+        List<ExprNode> right = coordinator.convertExpressionNode(node.getRhsExpressions());
+        resultSubstitutionNode = new AssignSubstitutionNode(getSourceCodePosition(node), left, right);
     }
 
     private SourceCodePosition getSourceCodePosition(Node node) {
