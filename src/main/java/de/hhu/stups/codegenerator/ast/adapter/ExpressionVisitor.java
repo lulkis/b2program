72b9eebce5d62e1b2dc6593d384de96800bbd0ca
Lukas Smyczek <slukasp@outlook.de>
2024-10-15 11:01:28 +0200

    Adding support for Interval Node

--- src/main/java/de/hhu/stups/codegenerator/ast/adapter/ExpressionVisitor.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/adapter/ExpressionVisitor.java
@@ -1,18 +1,24 @@
 package de.hhu.stups.codegenerator.ast.adapter;
 
+import de.be4.classicalb.core.parser.node.AIntervalExpression;
 import de.be4.classicalb.core.parser.node.Node;
 import de.be4.classicalb.core.parser.node.TIdentifierLiteral;
 import de.be4.classicalb.core.parser.node.TIntegerLiteral;
+import de.hhu.stups.codegenerator.ast.VisitorCoordinator;
 import de.prob.parser.ast.SourceCodePosition;
 import de.prob.parser.ast.nodes.expression.ExprNode;
+import de.prob.parser.ast.nodes.expression.ExpressionOperatorNode;
 import de.prob.parser.ast.nodes.expression.IdentifierExprNode;
 import de.prob.parser.ast.nodes.expression.NumberNode;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExpressionVisitor extends AbstractVisitor{
 
     private ExprNode resultExpressionNode;
+    private VisitorCoordinator coordinator = new VisitorCoordinator();
 
     public ExprNode getResult() {
         return resultExpressionNode;
@@ -26,6 +32,16 @@ public class ExpressionVisitor extends AbstractVisitor{
     @Override
     public void caseTIntegerLiteral(TIntegerLiteral node){
         resultExpressionNode = new NumberNode(getSourceCodePosition(node), new BigInteger(node.getText()));
+    }
+
+    @Override
+    public void caseAIntervalExpression(AIntervalExpression node){
+        List<ExprNode> intervalList = new ArrayList<>();
+        intervalList.add(coordinator.convertExpressionNode(node.getLeftBorder()));
+        intervalList.add(coordinator.convertExpressionNode(node.getRightBorder()));
+        resultExpressionNode = new ExpressionOperatorNode(getSourceCodePosition(node),
+                intervalList,
+                ExpressionOperatorNode.ExpressionOperator.INTERVAL);
     }
 
     private SourceCodePosition getSourceCodePosition(Node node) {
