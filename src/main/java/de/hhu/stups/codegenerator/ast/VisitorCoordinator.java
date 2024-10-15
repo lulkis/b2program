e3f6f3cd574b62e88c9ccb76a735a8f2b8adc79c
Lukas Smyczek <slukasp@outlook.de>
2024-10-15 09:24:40 +0200

    Adding ConvertExpressionNode Method to the VisitorCoordinator

--- src/main/java/de/hhu/stups/codegenerator/ast/VisitorCoordinator.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/VisitorCoordinator.java
@@ -1,15 +1,10 @@
 package de.hhu.stups.codegenerator.ast;
 
-import de.be4.classicalb.core.parser.node.POperation;
-import de.be4.classicalb.core.parser.node.PPredicate;
-import de.be4.classicalb.core.parser.node.PSubstitution;
-import de.be4.classicalb.core.parser.node.Start;
-import de.hhu.stups.codegenerator.ast.adapter.MachineVisitor;
-import de.hhu.stups.codegenerator.ast.adapter.OperationVisitor;
-import de.hhu.stups.codegenerator.ast.adapter.PredicateVisitor;
-import de.hhu.stups.codegenerator.ast.adapter.SubstitutionVisitor;
+import de.be4.classicalb.core.parser.node.*;
+import de.hhu.stups.codegenerator.ast.adapter.*;
 import de.prob.parser.ast.nodes.MachineNode;
 import de.prob.parser.ast.nodes.OperationNode;
+import de.prob.parser.ast.nodes.expression.ExprNode;
 import de.prob.parser.ast.nodes.predicate.PredicateNode;
 import de.prob.parser.ast.nodes.substitution.SubstitutionNode;
 import jdk.dynalink.Operation;
@@ -36,6 +31,12 @@ public class VisitorCoordinator {
 
     public OperationNode convertOperationNode(POperation node){
         OperationVisitor visitor = new OperationVisitor();
+        node.apply(visitor);
+        return visitor.getResult();
+    }
+
+    public ExprNode convertExpressionNode(PExpression node){
+        ExpressionVisitor visitor = new ExpressionVisitor();
         node.apply(visitor);
         return visitor.getResult();
     }
