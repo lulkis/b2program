c313ddb2f5b465f3d2b157589bbd56582a084261
Lukas Smyczek <slukasp@outlook.de>
2024-10-15 13:17:19 +0200

    Adding Support for multiple Substitution Nodes in SubstitutionVisitor

--- src/main/java/de/hhu/stups/codegenerator/ast/adapter/SubstitutionVisitor.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/adapter/SubstitutionVisitor.java
@@ -1,16 +1,16 @@
 package de.hhu.stups.codegenerator.ast.adapter;
 
-import de.be4.classicalb.core.parser.node.AAssignSubstitution;
-import de.be4.classicalb.core.parser.node.APreconditionSubstitution;
-import de.be4.classicalb.core.parser.node.Node;
+import de.be4.classicalb.core.parser.node.*;
 import de.hhu.stups.codegenerator.ast.VisitorCoordinator;
+import de.prob.parser.antlr.Util;
 import de.prob.parser.ast.SourceCodePosition;
+import de.prob.parser.ast.nodes.DeclarationNode;
 import de.prob.parser.ast.nodes.expression.ExprNode;
 import de.prob.parser.ast.nodes.predicate.PredicateNode;
-import de.prob.parser.ast.nodes.substitution.AssignSubstitutionNode;
-import de.prob.parser.ast.nodes.substitution.ConditionSubstitutionNode;
-import de.prob.parser.ast.nodes.substitution.SubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.*;
+import org.antlr.v4.runtime.Token;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SubstitutionVisitor extends AbstractVisitor{
@@ -37,6 +37,52 @@ public class SubstitutionVisitor extends AbstractVisitor{
                 ConditionSubstitutionNode.Kind.PRECONDITION,
                 condition,
                 substitution);
+    }
+
+    @Override
+    public void caseASelectSubstitution(ASelectSubstitution node){
+        resultSubstitutionNode = new IfOrSelectSubstitutionsNode(getSourceCodePosition(node),
+                IfOrSelectSubstitutionsNode.Operator.SELECT,
+                List.of(coordinator.convertPredicateNode(node.getCondition())),
+                List.of(coordinator.convertSubstitutionNode(node.getThen())),
+                coordinator.convertSubstitutionNode(node.getElse()));
+    }
+
+    @Override
+    public void caseAParallelSubstitution(AParallelSubstitution node){
+        resultSubstitutionNode = new ListSubstitutionNode(getSourceCodePosition(node),
+                ListSubstitutionNode.ListOperator.Parallel,
+                coordinator.convertSubstitutionNode(node.getSubstitutions()));
+    }
+
+    @Override
+    public void caseAChoiceOrSubstitution(AChoiceOrSubstitution node){
+        List<SubstitutionNode> choiceList = new ArrayList<>();
+        choiceList.add(coordinator.convertSubstitutionNode(node.getSubstitution()));
+        resultSubstitutionNode = new ChoiceSubstitutionNode(getSourceCodePosition(node),
+                choiceList);
+    }
+
+    @Override
+    public void caseASkipSubstitution(ASkipSubstitution node){
+        resultSubstitutionNode = new SkipSubstitutionNode(getSourceCodePosition(node));
+    }
+
+    @Override
+    public void caseAAnySubstitution(AAnySubstitution node){
+        PredicateNode predicate = coordinator.convertPredicateNode(node.getWhere());
+        SubstitutionNode substitution = coordinator.convertSubstitutionNode(node.getThen());
+        List<DeclarationNode> identifierList = new ArrayList<>();
+
+        for (PExpression expression : node.getIdentifiers()) {
+            String name = expression.toString().replace(" ", "");
+            DeclarationNode decl = new DeclarationNode(getSourceCodePosition(expression),
+                    name,
+                    DeclarationNode.Kind.SUBSTITUION_IDENTIFIER,
+                    null);
+            identifierList.add(decl);
+        }
+        resultSubstitutionNode = new AnySubstitutionNode(getSourceCodePosition(node), identifierList, predicate, substitution);
     }
 
     private SourceCodePosition getSourceCodePosition(Node node) {
