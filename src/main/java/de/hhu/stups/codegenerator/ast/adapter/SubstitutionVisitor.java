7ae88ad9dada3a8db6da80dbc419f830b1ccfd9e
Lukas Smyczek <slukasp@outlook.de>
2024-10-15 11:23:07 +0200

    Adding support for Precondition in SubstitutionVisitor

--- src/main/java/de/hhu/stups/codegenerator/ast/adapter/SubstitutionVisitor.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/adapter/SubstitutionVisitor.java
@@ -1,11 +1,14 @@
 package de.hhu.stups.codegenerator.ast.adapter;
 
 import de.be4.classicalb.core.parser.node.AAssignSubstitution;
+import de.be4.classicalb.core.parser.node.APreconditionSubstitution;
 import de.be4.classicalb.core.parser.node.Node;
 import de.hhu.stups.codegenerator.ast.VisitorCoordinator;
 import de.prob.parser.ast.SourceCodePosition;
 import de.prob.parser.ast.nodes.expression.ExprNode;
+import de.prob.parser.ast.nodes.predicate.PredicateNode;
 import de.prob.parser.ast.nodes.substitution.AssignSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.ConditionSubstitutionNode;
 import de.prob.parser.ast.nodes.substitution.SubstitutionNode;
 
 import java.util.List;
@@ -24,6 +27,16 @@ public class SubstitutionVisitor extends AbstractVisitor{
         List<ExprNode> left = coordinator.convertExpressionNode(node.getLhsExpression());
         List<ExprNode> right = coordinator.convertExpressionNode(node.getRhsExpressions());
         resultSubstitutionNode = new AssignSubstitutionNode(getSourceCodePosition(node), left, right);
+    }
+
+    @Override
+    public void caseAPreconditionSubstitution(APreconditionSubstitution node){
+        SubstitutionNode substitution = coordinator.convertSubstitutionNode(node.getSubstitution());
+        PredicateNode condition = coordinator.convertPredicateNode(node.getPredicate());
+        resultSubstitutionNode = new ConditionSubstitutionNode(getSourceCodePosition(node),
+                ConditionSubstitutionNode.Kind.PRECONDITION,
+                condition,
+                substitution);
     }
 
     private SourceCodePosition getSourceCodePosition(Node node) {
