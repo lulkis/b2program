2ed0dd8f1bced230397332a53bf05f1b13d5fb55
Lukas Smyczek <slukasp@outlook.de>
2024-10-15 09:21:58 +0200

    Adding ConvertSubstitutionNode Method to the VisitorCoordinator

--- src/main/java/de/hhu/stups/codegenerator/ast/VisitorCoordinator.java
+++ src/main/java/de/hhu/stups/codegenerator/ast/VisitorCoordinator.java
@@ -1,14 +1,23 @@
 package de.hhu.stups.codegenerator.ast;
 
+import de.be4.classicalb.core.parser.node.PSubstitution;
 import de.be4.classicalb.core.parser.node.Start;
 import de.hhu.stups.codegenerator.ast.adapter.MachineVisitor;
+import de.hhu.stups.codegenerator.ast.adapter.SubstitutionVisitor;
 import de.prob.parser.ast.nodes.MachineNode;
+import de.prob.parser.ast.nodes.substitution.SubstitutionNode;
 
 public class VisitorCoordinator {
 
     public MachineNode convertMachineNode(Start start){
         MachineVisitor visitor = new MachineVisitor();
         start.apply(visitor);
+        return visitor.getResult();
+    }
+
+    public SubstitutionNode convertSubstitutionNode(PSubstitution node){
+        SubstitutionVisitor visitor = new SubstitutionVisitor();
+        node.apply(visitor);
         return visitor.getResult();
     }
 }
