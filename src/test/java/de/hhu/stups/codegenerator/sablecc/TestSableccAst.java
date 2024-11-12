819160d1a5d7c7750742431b69a123bb9ccfa0b8
Lukas Smyczek <slukasp@outlook.de>
2024-11-12 23:20:55 +0100

    Making all Tests better

--- src/test/java/de/hhu/stups/codegenerator/sablecc/TestSableccAst.java
+++ src/test/java/de/hhu/stups/codegenerator/sablecc/TestSableccAst.java
@@ -4,9 +4,11 @@ import de.be4.classicalb.core.parser.BParser;
 import de.be4.classicalb.core.parser.node.Start;
 import de.hhu.stups.codegenerator.CodeGenerator;
 import de.hhu.stups.codegenerator.ast.VisitorCoordinator;
+import de.hhu.stups.codegenerator.ast.SableCCBParser;
 import de.hhu.stups.codegenerator.ast.nodes.MachineNodeWithDefinitions;
 import de.prob.parser.antlr.Antlr4BParser;
 import de.prob.parser.antlr.BProject;
+import de.prob.parser.ast.nodes.DeclarationNode;
 import de.prob.parser.ast.nodes.MachineNode;
 import de.prob.parser.ast.nodes.OperationNode;
 import org.junit.Test;
@@ -18,200 +20,143 @@ import java.nio.file.Paths;
 public class TestSableccAst {
 
     @Test
-    public void testCorrectMachineNameLift() throws Exception{
-        BParser parser = new BParser();
+    public void testLift() throws Exception{
         Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
                 .getResource("de/hhu/stups/codegenerator/sablecc/Lift.mch").toURI());
-        Start start = parser.parseFile(mchPath.toFile());
-        VisitorCoordinator coordinator = new VisitorCoordinator();
-        MachineNode machineNode;
 
-        BProject antlrMachineNode  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
-        machineNode = coordinator.convertMachineNode(start);
 
-        assertEquals(antlrMachineNode.getMachineNode("Lift").getName(),
-                machineNode.getName());
+        BProject antlrProject  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
+        BProject sableProject = SableCCBParser.createBProjectFromMainMachineFile(mchPath.toFile());
+        MachineNode antlrNode = antlrProject.getMainMachine();
+        MachineNode sableNode = sableProject.getMainMachine();
+
+
+        assertEquals(antlrNode.getName(), sableNode.getName());
+        int c = 0;
+        for(DeclarationNode p : antlrNode.getVariables()){
+            assertEquals(p.toString(), sableNode.getVariables().get(c).toString());
+            c++;
+        }
+        assertEquals(antlrNode.getInitialisation().toString(), sableNode.getInitialisation().toString());
+        assertEquals(antlrNode.getInvariant().toString(), sableNode.getInvariant().toString());
+        c = 0;
+        for(OperationNode p : antlrNode.getOperations()){
+            assertEquals(p.toString(), sableNode.getOperations().get(c).toString());
+            c++;
+        }
     }
 
     @Test
-    public void testVariablesLift() throws Exception {
-        BParser parser = new BParser();
-        Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
-                .getResource("de/hhu/stups/codegenerator/sablecc/Lift.mch").toURI());
-        Start start = parser.parseFile(mchPath.toFile());
-        VisitorCoordinator coordinator = new VisitorCoordinator();
-        MachineNode machineNode;
-
-        BProject antlrMachineNode  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
-        machineNode = coordinator.convertMachineNode(start);
-
-        assertEquals(antlrMachineNode.getMachineNode("Lift").getVariables().size(),
-                machineNode.getVariables().size());
-    }
-
-    @Test
-    public void testInitialisationLift() throws Exception {
-        BParser parser = new BParser();
-        Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
-                .getResource("de/hhu/stups/codegenerator/sablecc/Lift.mch").toURI());
-        Start start = parser.parseFile(mchPath.toFile());
-        VisitorCoordinator coordinator = new VisitorCoordinator();
-        MachineNode machineNode;
-
-        BProject antlrMachineNode  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
-        machineNode = coordinator.convertMachineNode(start);
-
-        assertEquals(antlrMachineNode.getMachineNode("Lift").getInitialisation().toString(),
-                machineNode.getInitialisation().toString());
-    }
-
-    @Test
-    public void testInvariantLift() throws Exception {
-        BParser parser = new BParser();
-        Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
-                .getResource("de/hhu/stups/codegenerator/sablecc/Lift.mch").toURI());
-        Start start = parser.parseFile(mchPath.toFile());
-        VisitorCoordinator coordinator = new VisitorCoordinator();
-        MachineNode machineNode;
-
-        BProject antlrMachineNode  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
-        machineNode = coordinator.convertMachineNode(start);
-
-        assertEquals(antlrMachineNode.getMachineNode("Lift").getInvariant().toString(),
-                machineNode.getInvariant().toString());
-    }
-
-    @Test
-    public void testOperationsLift() throws Exception{
-        BParser parser = new BParser();
-        Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
-                .getResource("de/hhu/stups/codegenerator/sablecc/Lift.mch").toURI());
-        Start start = parser.parseFile(mchPath.toFile());
-        VisitorCoordinator coordinator = new VisitorCoordinator();
-        MachineNode machineNode;
-
-        BProject antlrMachineNode  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
-        machineNode = coordinator.convertMachineNode(start);
-
-        assertEquals(antlrMachineNode.getMachineNode("Lift").getOperations().get(0).toString(),
-                machineNode.getOperations().get(0).toString());
-    }
-
-    @Test
-    public void testCorrectMachineNameNota() throws Exception{
-        BParser parser = new BParser();
+    public void testNota() throws Exception{
         Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
                 .getResource("de/hhu/stups/codegenerator/sablecc/nota.mch").toURI());
-        Start start = parser.parseFile(mchPath.toFile());
-        VisitorCoordinator coordinator = new VisitorCoordinator();
-        MachineNode machineNode;
 
-        //BProject antlrMachineNode  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
-        machineNode = coordinator.convertMachineNode(start);
+        BProject antlrProject  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
+        BProject sableProject = SableCCBParser.createBProjectFromMainMachineFile(mchPath.toFile());
+        MachineNode antlrNode = antlrProject.getMainMachine();
+        MachineNode sableNode = sableProject.getMainMachine();
 
-        assertEquals("nota",
-                machineNode.getName());
+        assertEquals(antlrNode.getName(), sableNode.getName());
+        assertEquals(antlrNode.getVariables().size(), sableNode.getVariables().size());
+        assertEquals(antlrNode.getEnumeratedSets().size(), sableNode.getEnumeratedSets().size());
+        assertEquals(antlrNode.getInitialisation().toString(), sableNode.getInitialisation().toString());
+        assertEquals(antlrNode.getInvariant().toString(), sableNode.getInvariant().toString());
+        assertEquals(antlrNode.getOperations().size(), sableNode.getOperations().size());
+        assertEquals(antlrNode.getDefinitions().size(), sableNode.getDefinitions().size());
     }
 
     @Test
-    public void testInvariantNota() throws Exception{
-        BParser parser = new BParser();
-        Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
-                .getResource("de/hhu/stups/codegenerator/sablecc/nota.mch").toURI());
-        Start start = parser.parseFile(mchPath.toFile());
-        VisitorCoordinator coordinator = new VisitorCoordinator();
-        MachineNode machineNode;
-
-        BProject antlrMachineNode  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
-        machineNode = coordinator.convertMachineNode(start);
-
-        assertEquals(antlrMachineNode.getMachineNode("nota").getInvariant().toString(),
-                machineNode.getInvariant().toString());
-    }
-
-    @Test
-    public void testCorrectMachineNameDefTest() throws Exception{
-        BParser parser = new BParser();
-        Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
+    public void testDefinitionTest() throws Exception{
+        Path mchPathSableCC = Paths.get(CodeGenerator.class.getClassLoader()
                 .getResource("de/hhu/stups/codegenerator/sablecc/DefTest.mch").toURI());
-        Start start = parser.parseFile(mchPath.toFile());
-        VisitorCoordinator coordinator = new VisitorCoordinator();
-
-        MachineNodeWithDefinitions machineNode;
-
-        //BProject antlrMachineNode  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
-        machineNode = (MachineNodeWithDefinitions) coordinator.convertMachineNode(start, parser.getDefinitions());
-
-        assertEquals("DefTest",
-                machineNode.getName());
-    }
-
-    @Test
-    public void testCorrectMachineNameDefTestExchange() throws Exception{
-        BParser parser = new BParser();
-        Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
-                .getResource("de/hhu/stups/codegenerator/sablecc/DefTest.mch").toURI());
-        Path mchPathCompare = Paths.get(CodeGenerator.class.getClassLoader()
+        Path mchPathANTLR = Paths.get(CodeGenerator.class.getClassLoader()
                 .getResource("de/hhu/stups/codegenerator/sablecc/DefTestComp.mch").toURI());
-        Start start = parser.parseFile(mchPath.toFile());
-        VisitorCoordinator coordinator = new VisitorCoordinator();
 
-        MachineNodeWithDefinitions machineNode;
+        BProject antlrProject = Antlr4BParser.createBProjectFromMainMachineFile(mchPathANTLR.toFile());
+        BProject sableProject = SableCCBParser.createBProjectFromMainMachineFile(mchPathSableCC.toFile());
+        MachineNode antlrNode = antlrProject.getMainMachine();
+        MachineNode sableNode = sableProject.getMainMachine();
 
-        BProject antlrMachineNode  = Antlr4BParser.createBProjectFromMainMachineFile(mchPathCompare.toFile());
-        machineNode = (MachineNodeWithDefinitions) coordinator.convertMachineNode(start, parser.getDefinitions());
+        assertEquals(antlrNode.getVariables().size(), sableNode.getVariables().size());
+        assertEquals(antlrNode.getEnumeratedSets().size(), sableNode.getEnumeratedSets().size());
+        assertEquals(antlrNode.getInitialisation().toString(), sableNode.getInitialisation().toString());
+        assertEquals(antlrNode.getInvariant().toString(), sableNode.getInvariant().toString());
+        assertEquals(antlrNode.getOperations().size(), sableNode.getOperations().size());
+    }
 
-        assertEquals(antlrMachineNode.getMachineNode("DefTestComp").getOperations().get(1).toString(),
-                machineNode.getOperations().get(1).toString());
+    @Test
+    public void testLandingGear() throws Exception{
+        Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
+                .getResource("de/hhu/stups/codegenerator/sablecc/LandingGear_R6.mch").toURI());
+
+        BProject antlrProject  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
+        BProject sableProject = SableCCBParser.createBProjectFromMainMachineFile(mchPath.toFile());
+        MachineNode antlrNode = antlrProject.getMainMachine();
+        MachineNode sableNode = sableProject.getMainMachine();
+
+        assertEquals(antlrNode.getName(), sableNode.getName());
+        assertEquals(antlrNode.getVariables().size(), sableNode.getVariables().size());
+        assertEquals(antlrNode.getEnumeratedSets().size(), sableNode.getEnumeratedSets().size());
+        assertEquals(antlrNode.getInitialisation().toString(), sableNode.getInitialisation().toString());
+        assertEquals(antlrNode.getInvariant().toString(), sableNode.getInvariant().toString());
+        assertEquals(antlrNode.getOperations().size(), sableNode.getOperations().size());
+        assertEquals(antlrNode.getDefinitions().size(), sableNode.getDefinitions().size());
+    }
+
+    @Test
+    public void testCruiseControl() throws Exception{
+        Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
+                .getResource("de/hhu/stups/codegenerator/sablecc/Cruise_finite1.mch").toURI());
+
+        BProject antlrProject  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
+        BProject sableProject = SableCCBParser.createBProjectFromMainMachineFile(mchPath.toFile());
+        MachineNode antlrNode = antlrProject.getMainMachine();
+        MachineNode sableNode = sableProject.getMainMachine();
+
+        assertEquals(antlrNode.getName(), sableNode.getName());
+        assertEquals(antlrNode.getVariables().size(), sableNode.getVariables().size());
+        assertEquals(antlrNode.getEnumeratedSets().size(), sableNode.getEnumeratedSets().size());
+        assertEquals(antlrNode.getInitialisation().toString(), sableNode.getInitialisation().toString());
+        assertEquals(antlrNode.getInvariant().toString(), sableNode.getInvariant().toString());
+        assertEquals(antlrNode.getOperations().size(), sableNode.getOperations().size());
+        assertEquals(antlrNode.getDefinitions().size(), sableNode.getDefinitions().size());
+    }
+
+    @Test
+    public void testGenerated100() throws Exception{
+        Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
+                .getResource("de/hhu/stups/codegenerator/sablecc/Generated100.mch").toURI());
+
+        BProject antlrProject  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
+        BProject sableProject = SableCCBParser.createBProjectFromMainMachineFile(mchPath.toFile());
+        MachineNode antlrNode = antlrProject.getMainMachine();
+        MachineNode sableNode = sableProject.getMainMachine();
+
+        assertEquals(antlrNode.getName(), sableNode.getName());
+        assertEquals(antlrNode.getVariables().size(), sableNode.getVariables().size());
+        assertEquals(antlrNode.getEnumeratedSets().size(), sableNode.getEnumeratedSets().size());
+        assertEquals(antlrNode.getInitialisation().toString(), sableNode.getInitialisation().toString());
+        assertEquals(antlrNode.getInvariant().toString(), sableNode.getInvariant().toString());
+        assertEquals(antlrNode.getOperations().size(), sableNode.getOperations().size());
+        assertEquals(antlrNode.getDefinitions().size(), sableNode.getDefinitions().size());
     }
 
     @Test
     public void testTrafficLight() throws Exception{
-        BParser parser = new BParser();
         Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
                 .getResource("de/hhu/stups/codegenerator/sablecc/TrafficLight.mch").toURI());
-        Start start = parser.parseFile(mchPath.toFile());
-        VisitorCoordinator coordinator = new VisitorCoordinator();
 
-        MachineNodeWithDefinitions machineNode;
+        BProject antlrProject  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
+        BProject sableProject = SableCCBParser.createBProjectFromMainMachineFile(mchPath.toFile());
+        MachineNode antlrNode = antlrProject.getMainMachine();
+        MachineNode sableNode = sableProject.getMainMachine();
 
-        BProject antlrMachineNode  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
-        machineNode = (MachineNodeWithDefinitions) coordinator.convertMachineNode(start, parser.getDefinitions());
-
-        assertEquals(antlrMachineNode.getMachineNode("TrafficLight").getInvariant().toString(),
-                machineNode.getInvariant().toString());
-    }
-
-    @Test
-    public void testTrafficLightOperations() throws Exception{
-        BParser parser = new BParser();
-        Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
-                .getResource("de/hhu/stups/codegenerator/sablecc/TrafficLight.mch").toURI());
-        Start start = parser.parseFile(mchPath.toFile());
-        VisitorCoordinator coordinator = new VisitorCoordinator();
-
-        MachineNodeWithDefinitions machineNode;
-
-        BProject antlrMachineNode  = Antlr4BParser.createBProjectFromMainMachineFile(mchPath.toFile());
-        machineNode = (MachineNodeWithDefinitions) coordinator.convertMachineNode(start, parser.getDefinitions());
-
-        assertEquals(antlrMachineNode.getMachineNode("TrafficLight").getOperations().size(),
-                machineNode.getOperations().size());
-    }
-
-    @Test
-    public void testBMachineNames() throws Exception{
-        BParser parser = new BParser();
-        Path mchPath = Paths.get(CodeGenerator.class.getClassLoader()
-                .getResource("de/hhu/stups/codegenerator/sablecc/BMachine.mch").toURI());
-        Start start = parser.parseFile(mchPath.toFile());
-        VisitorCoordinator coordinator = new VisitorCoordinator();
-
-        MachineNodeWithDefinitions machineNode;
-
-        machineNode = (MachineNodeWithDefinitions) coordinator.convertMachineNode(start, parser.getDefinitions());
-
-        assertEquals("BMachine",
-                machineNode.getName());
+        assertEquals(antlrNode.getName(), sableNode.getName());
+        assertEquals(antlrNode.getVariables().size(), sableNode.getVariables().size());
+        assertEquals(antlrNode.getEnumeratedSets().size(), sableNode.getEnumeratedSets().size());
+        assertEquals(antlrNode.getInitialisation().toString(), sableNode.getInitialisation().toString());
+        assertEquals(antlrNode.getInvariant().toString(), sableNode.getInvariant().toString());
+        assertEquals(antlrNode.getOperations().size(), sableNode.getOperations().size());
+        assertEquals(antlrNode.getDefinitions().size(), sableNode.getDefinitions().size());
     }
 }
